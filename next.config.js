488a01ff9a9e446f1c8a198be8a45e56754cbd81
yxj1716643958-lgtm <yxj1716643958@gmail.com>
2025-12-12 13:47:10 +0800

    Fix Vercel deployment configuration and build issues

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
+}
 
 module.exports = nextConfig
